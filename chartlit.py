84ef850f2748d2cb9248dbd99b5d6983b48991bc
deep1401 <gandhi0869@gmail.com>
2020-03-21 18:19:54 +0530

    Progress bar fix, shit logic

--- chartlit.py
+++ chartlit.py
@@ -11,9 +11,20 @@ chart=st.line_chart([float(df1['year'][0]),float(df1['yield'][0])])
 
 for i in range(len(df1)):
     last_row=[float(df1['year'][i]),float(df1['yield'][i])]
-    status_text.text(f'{i}% Complete')
+
     chart.add_rows(last_row)
-    progress_bar.progress(int((i/len(df1))*100))
+    if(i<20):
+        status_text.text('0% Complete')
+        progress_bar.progress(0)
+    elif i<99:
+        status_text.text(f'{i}% Complete')
+        progress_bar.progress(i)
+    elif i<(len(df1)-2):
+        status_text.text('99% Complete')
+        progress_bar.progress(99)
+    else:
+        status_text.text(f'100% Complete')
+        progress_bar.progress(100)
     time.sleep(0.07)
 
 progress_bar.empty()
